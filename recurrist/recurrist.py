f40e2e8652dfee738f0d2228123ac518ac26e81b
Gregor <gheim314+github@gmail.com>
2020-11-17 19:58:18 +0100

    Basic structure, configuration, requirements

---
+++ recurrist/recurrist.py
@@ -0,0 +1,8 @@
+import sys
+from todoist.api import TodoistAPI
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(
     name='Recurrist',
     version='0.1dev',
+    packages=['recurrist',],
     long_description=open('README.md').read(),
 )
